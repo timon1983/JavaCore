6bb7f5533ba2e5209ab50c11f6aa94baa47d633c
Timon1983 <timon1983@yandex.ru>
2021-02-23 14:01:24 +0500

    update Foo

--- src/main/java/com/timon1983/javacore/Task3/Foo.java
+++ src/main/java/com/timon1983/javacore/Task3/Foo.java
@@ -2,36 +2,81 @@ package Task3;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Foo {
 
-    public void first(){
+    Semaphore semaphore1 = new Semaphore(0);
+    Semaphore semaphore2 = new Semaphore(1);
+    Semaphore semaphore3 = new Semaphore(0);
+
+
+
+    public synchronized void first() {
+        try {
+            semaphore2.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.print("First");
+        semaphore1.release();
+        semaphore2.release();
     }
 
-    public void second(){
-        System.out.print("Second");
-    }
+        public synchronized void second() {
+            try {
+                semaphore1.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                semaphore2.acquire();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.print("Second");
+            semaphore2.release();
+            semaphore3.release();
 
-    public void third(){
-        System.out.print("Third");
+    }
+            public synchronized void third() {
+                try {
+                    semaphore3.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    semaphore2.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.print("Third");
+                semaphore2.release();
     }
 }
 
+
+
+
+
 class FooMain{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Foo foo = new Foo();
-        CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
-            foo.first();
-            foo.second();
+
+        CompletableFuture.runAsync(() -> {
+
             foo.third();
         });
-        try {
-            completableFuture.get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+
+        CompletableFuture.runAsync(() -> {
+
+            foo.first();
+        });
+        CompletableFuture.runAsync(() -> {
+
+            foo.second();
+        });
     }
 }
